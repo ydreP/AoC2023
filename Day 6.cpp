f4faad7b31c1659ecbf330014707abda514e0b45
ydreP <140316236+ydreP@users.noreply.github.com>
2023-12-18 21:56:07 +0100

    Update Day 6.cpp
    Cleaning

--- Day 6.cpp
+++ Day 6.cpp
@@ -1,21 +1,8 @@
-// Day 6.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <string> 
 #include <vector>
 #include <fstream>
 #include <sstream>
-#include <cmath>
-#include <set>
-#include <map>
-#include <algorithm>
-#include <climits>
-#include <queue>
-#include <cctype>
-#include <regex>
-#include <tuple>
-
 std::vector<long long> numberOfVictories(std::vector< long long> race_durations, std::vector< long long> records) {
     std::vector< long long > number_of_victories;
     for (int i = 0; i < race_durations.size(); i++) {
@@ -24,7 +11,6 @@ std::vector<long long> numberOfVictories(std::vector< long long> race_durations,
     }
     return number_of_victories;
 }
-
 int main() {
     std::vector<long long > test_duration = {71530}; 
     std:: vector<long long> test_records = {940200};
@@ -41,14 +27,3 @@ int main() {
     std::cout << "\n";
     std::cout << "Number of ways to win : " << prod; 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
